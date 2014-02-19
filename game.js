81005dbb7876081b1227815ba89e0244680eca09
Michalski Grzegorz <grz.michalski@gmail.com>
2014-02-19 12:13:39 +0100

    003 commit
    add:
    - comments

--- game.js
+++ game.js
@@ -5,7 +5,7 @@
 // add: Michalski Grzegorz
 
 // User interaction. Choose one of three options.
-var userChoice = prompt("Do you choose rock, paper or scissors?");
+var userChoice = prompt("Hi. Please think and write. Do you choose rock, paper or scissors?");
 
 // Set random variable
 var computerChoice = Math.random();
@@ -19,14 +19,14 @@ if (computerChoice < 0.34) {
 	computerChoice = "scissors";
 }
 
-// komunikat dotyczacy wyboru, co wybral user i co wybrala maszyna
+// This log showed choices
 var choose = function (userChoice, computerChoice) {
     console.log("You select: " + userChoice + " // " + "Computer select: " + computerChoice);
 };
 
-// funkcja porownawcza
+// Compare function
 var compare = function (choice1, choice2) {
-// warunek - jezeli maszyna i user wybiora to samo, to remis        
+// condition - if computer and user choices that the same, then it's draw
         if (choice1 == choice2) {
             return "The result is a tie! Let's play again :)"; 
         }        
@@ -53,13 +53,13 @@ var compare = function (choice1, choice2) {
                     return "scissors wins";
                 }
             }
-// warunek - jezeli user wpisze cos innego niz papier, kamien, nozyczki            
+// condition - if user answered some other words, not rock, paper or scissors
             if (userChoice !== "rock" || userChoice !== "paper" || userChoice !== "scissors") {
         console.log("Do you know rules in this game?. Please select with only three options: rock, paper, scissors.");
             }
 };
 
-// wywolania funckji
+// call functions
 choose(userChoice, computerChoice);
 compare(userChoice, computerChoice);
 // compare(rock, rock);
